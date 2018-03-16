3b644823d103f2e986c28922e9482323081a882d
Lauri Rooden <lauri@rooden.ee>
2018-03-16 22:39:17 +0200

    path: Rewrite to string walk

--- path.js
+++ path.js
@@ -7,19 +7,58 @@
 	exports.resolve = resolve
 
 	function normalize(path) {
-		var arr = path.split(/\/+/)
-		, i = arr.length
+		var c
+		, SLASH = 47
+		, DOT = 46
+		, res = path
+		, abs = res.charCodeAt(0) == SLASH
+		, len = res.length
+		, i = len
+		, cut = len
 		, up = 0
-		, abs = path.charAt(0) == "/"
-		for (; i--; ) {
-			if (arr[i] == "." || arr[i] == ".." && ++up || up && up--) {
-				arr.splice(i, 1)
+		, last = SLASH
+
+		for (; i > 0; ) {
+			c = res.charCodeAt(--i)
+			if (c === SLASH) {
+				if (cut === 0) {
+					cut = i
+				} else if (last !== SLASH && up > 0) {
+					up--
+				}
+			} else if (last === SLASH) {
+				if (c === DOT) {
+					if (i > 0) {
+						c = res.charCodeAt(--i)
+						if (c === DOT && (i === 0 || res.charCodeAt(i - 1) === SLASH)) {
+							last = SLASH
+							i--
+							up++
+						} else if (c !== SLASH) {
+							cut = 0
+						}
+						continue
+					} else {
+						break
+					}
+				}
+				if (up === 0 && cut > 0) {
+					if (len === cut) {
+						if (cut > i + 2) {
+							res = res.slice(0, res.charCodeAt(len - 1) === SLASH ? i + 2 : i + 1)
+						}
+					} else if (cut > i + 1) {
+						res = res.slice(0, i + 1) + res.slice(cut)
+					}
+					cut = 0
+				}
 			}
+			last = c
 		}
-		if (!abs) for (; up--; ) {
-			arr.unshift("..")
+		if (cut > 0 && i === 0) {
+			res = res.slice(abs ? cut : cut + 1)
 		}
-		return (abs && arr[0] ? sep : "") + arr.join(sep)
+		return res
 	}
 
 	function relative(from, to) {
@@ -45,7 +84,7 @@
 		, out = []
 
 		for (; i; ) {
-			if ((out[i--] = clear(args[i])).charAt(0) == "/") {
+			if ((out[i--] = clear(args[i])).charCodeAt(0) === 47) {
 				out.splice(0, i + 1)
 				i = 0
 			}
@@ -56,8 +95,8 @@
 	}
 
 	function clear(path) {
-		if (typeof path != "string") {
-			throw new TypeError("Path must be a string. Received " + typeof path)
+		if (typeof path !== "string") {
+			throw new TypeError("Path must be a string. Got " + typeof path)
 		}
 		return path.replace(/\/+$/, "")
 	}
